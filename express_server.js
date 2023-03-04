52949b5a8f568edfb3c27b03dd8c30242fb0ca0b
Ekaterina Shilina <e.shilina@Ekaterinas-MacBook-Pro.local>
2023-03-03 22:43:15 -0500

    Add a POST route that removes a URL resource: POST /urls/:id/delete.

--- express_server.js
+++ express_server.js
@@ -32,6 +32,12 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post('/urls/:id/delete', (req, res) => {
+  const shortURL = req.params.id;
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
