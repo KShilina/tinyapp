0ed46dba16fb9acea885fdb70f0ff717ca4db8dc
Ekaterina Shilina <e.shilina@Ekaterinas-MacBook-Pro.local>
2023-03-05 14:51:23 -0500

    Pass username to each EJS template/implement cookieParser.

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const app = express();
 const PORT = 8080; // default port 8080
+
+app.use(cookieParser())
 
 app.set("view engine", "ejs");
 
@@ -16,7 +19,7 @@ function generateRandomString() {
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase,  username: req.cookies["username"], };
   res.render("urls_index", templateVars);
 });
 
@@ -29,11 +32,14 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id],  username: req.cookies["username"], };
   res.render("urls_show", templateVars);
 });
 
