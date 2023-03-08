d1e409737b2ab5bd5d51d3dd16147551467e0694
Ekaterina Shilina <e.shilina@Ekaterinas-MacBook-Pro.local>
2023-03-08 12:47:04 -0500

    Create a GET /login endpoint.

--- express_server.js
+++ express_server.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 const users = {
@@ -28,7 +28,7 @@ const users = {
 
 // function to generate random short URL
 function generateRandomString() {
-  return result = Math.random().toString(36).substring(2,8);
+  return (result = Math.random().toString(36).substring(2, 8));
 }
 
 app.get("/urls", (req, res) => {
@@ -56,37 +56,44 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   let userID = req.cookies["user_id"];
   let userData = users[userID];
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id],  user: userData };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    user: userData,
+  };
   res.render("urls_show", templateVars);
 });
 
-app.post('/urls/:id/delete', (req, res) => {
+app.post("/urls/:id/delete", (req, res) => {
   const shortURL = req.params.id;
   delete urlDatabase[shortURL];
-  res.redirect('/urls');
+  res.redirect("/urls");
 });
 
-//route handler for login 
-app.post('/login', (req, res) => {
+//route handler for login
+app.post("/login", (req, res) => {
   let username = req.body.username;
-  const userObj = Object.entries(users).find(([id,user]) => user.email === username)
-  console.log("Test1:",users);
-  console.log("Test1:",userObj);
+  const userObj = Object.entries(users).find(
+    ([id, user]) => user.email === username
+  );
+  console.log("Test1:", users);
+  console.log("Test1:", userObj);
   res.cookie("user_id", userObj[0]);
-  res.redirect('/urls');
+  //console.log("Request cookies:",req.cookies);
+  res.redirect("/urls");
 });
 
 //POST route for /logout which clears the cookie and redirects the user to /urls page
-app.post('/logout', (req, res) => {
-  console.log("Test",req.body)
-  res.clearCookie("user_id")
-  res.redirect('/urls');
+app.post("/logout", (req, res) => {
+  console.log("Test", req.body);
+  res.clearCookie("user_id");
+  res.redirect("/urls");
 });
 
-app.post('/urls/:id/edit', (req, res) => {
+app.post("/urls/:id/edit", (req, res) => {
   const shortURL = req.params.id;
-  urlDatabase[shortURL] = req.body.longURL
-  res.redirect('/urls');
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect("/urls");
 });
 
 app.get("/u/:id", (req, res) => {
@@ -100,17 +107,26 @@ app.get("/register", (req, res) => {
   let userID = req.cookies["user_id"];
   let userData = users[userID];
   const templateVars = { urls: urlDatabase, user: userData };
-  res.render("register",templateVars);
+  res.render("register", templateVars);
 });
 
 // route handler for registration
-app.post("/register", (req,res) => {
-  const {email, password} = req.body;
+app.post("/register", (req, res) => {
+  const { email, password } = req.body;
   if (!email || !password) {
     res.status(400).send("Invalid email or password!");
-    
+    return;
   }
 
+  // userRandomID = id; small object inside the users object = user
+  const userObj = Object.entries(users).find(
+    ([id, user]) => user.email === email
+  );
+  const foundEmail = Boolean(userObj);
+  if (foundEmail) {
+    res.status(400).send("Email is already exist!");
+    return;
+  }
   const userID = generateRandomString();
   // Add the key-value pair to the database
   users[userID] = {
@@ -118,15 +134,22 @@ app.post("/register", (req,res) => {
     email: email,
     password: password,
   };
-  res.cookie("user_id", userID)
-  console.log("Users:",users)
+  res.cookie("user_id", userID);
+  console.log("Users:", users);
   let user = users[userID];
 
   //console.log("Database:",users);
-  
-  const templateVars = { urls: urlDatabase, user: user };
-  res.redirect('/urls');
 
+  const templateVars = { urls: urlDatabase, user: user };
+  res.redirect("/urls");
+});
+
+app.get("/login", (req, res) => {
+  let userID = req.cookies["user_id"];
+  let user = users[userID];
+  const templateVars = {user};
+
+  res.render("login", templateVars)
 })
 
 app.get("/", (req, res) => {
@@ -140,11 +163,6 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
-
-
-
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
